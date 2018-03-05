4dad62b6be1ec588e3d58d15cee309c67723aa07
Colin Barry <colinsbarry@gmail.com>
2018-03-05 15:20:40 -0500

    Crawls recursively, grabs more product info, adds timeout

--- hmbdy/hmbdy_target.py
+++ hmbdy/hmbdy_target.py
@@ -6,8 +6,11 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 
 import time
+import json
+import re
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+
 
 # set to mweb
 opts = Options()
@@ -15,21 +18,68 @@ opts = Options()
 opts.add_argument(" — incognito")
 
 browser = webdriver.Chrome(executable_path='/Library/Application Support/Google/chromedriver', chrome_options=opts)
+browser.set_script_timeout(15)
 
-# set up product class
-class product:
-    def __init__(self, sku, name, lead_image, price, review_stars, review_count):
-        self.sku = sku or ''
-        self.name = name or ''
-        self.lead_image = lead_image or ''
-        self.price = price or ''
-        self.review_stars = review_stars or ''
-        self.review_count = review_count or ''
+# scrape the products on a given PLP
+def scrape_PLP(base_url):
+    pdp_links = {}
+    browser.get(base_url)
+    time.sleep(1)
 
+    webdriver.ActionChains(browser).send_keys(Keys.ESCAPE).perform()
+    for i in range(1, 10):
+        browser.execute_script("window.scrollBy(0, 500);")
+        time.sleep(1)
+
+    product_cards = browser.find_elements_by_xpath('//div[@data-test="product-card"]/div[1]/h3[1]/a[1]')
+    print(len(product_cards))
+
+    pdp_links = [x.get_attribute("href") for x in product_cards]
+    print(pdp_links, '\n')
+
+    # scrape every PDP on this page
+    product_infos = []
+    for link in pdp_links:
+        prod_page = browser.get(link)
+        for i in range(1, 1):
+            browser.execute_script("window.scrollBy(0, 500);")
+            time.sleep(1)
+        browser.find_element_by_xpath('//*[@id="tabContent-tab-details"]/div/div[3]/button').click()
+        product_infos.append(get_product_info(prod_page))
+
+    return product_infos
+
+# scrape the product info on a given PDP
 def get_product_info(product_page):
     product_facts = {}
+    # get product name
     name = browser.find_element_by_xpath('//*[@id="mainContainer"]/div/div/div[1]/div/div[1]/h1/span').text
     product_facts["product_name"] = name
+
+    # get product URL
+    product_facts["product_url"] = browser.current_url
+
+    # get product SKU
+    product_facts["SKU"] = re.search('(?<=\/A-)\d{2,15}', browser.current_url).group(0)
+
+    # get product lead image URL
+    image_url = browser.find_element_by_xpath('//div[@data-test="filmstrip"]/div/ul/li[1]/button/img').get_attribute('src')
+    product_facts["product_lead_image"] = image_url
+
+    # get product price
+    price = browser.find_element_by_xpath('//div[@data-test="product-price"]/span').text
+
+    # get rating count
+    product_facts["rating_count"] = browser.find_element_by_xpath('//span[@data-test="ratingCount"]/span').text
+
+    # get average rating
+    rating_length = browser.find_element_by_xpath('//div[@data-ref="rating-mask"]').get_attribute('style')
+    try:
+        product_facts["average_rating"] = re.search('(?<=width: )\d{1,3}', rating_length).group(0)
+    except:
+        product_facts["average_rating"] = '0'
+
+    # get product description
     product_fact_div = browser.find_elements_by_xpath('//*[@id="tabContent-tab-details"]/div/div[1]/*')
     #first div is always product description
     product_facts["description"] = product_fact_div[0].text
@@ -42,46 +92,29 @@ def get_product_info(product_page):
     print(product_facts)
     return product_facts
 
-def get_product_image(product_page):
-    pass
-
-productList = []
-
-browser.get("https://www.target.com/s?searchTerm=chairs&sortBy=relevance&Nao=0&limit=6")
-
-time.sleep(1)
-
-webdriver.ActionChains(browser).send_keys(Keys.ESCAPE).perform()
-for i in range(1, 10):
-    browser.execute_script("window.scrollBy(0, 500);")
-    time.sleep(1)
-
-# find_elements_by_xpath returns an array of selenium objects.
-product_cards = browser.find_elements_by_xpath('//div[@data-test="product-card"]/div[1]/h3[1]/a[1]')
-print(len(product_cards))
-
-links = [x.get_attribute("href") for x in product_cards]
-print(links, '\n')
-
+# start here
 product_infos = []
+base_url = "https://www.target.com/c/chairs-living-room-furniture/-/N-5xtlz?limit=96&Nao=0"
+#base_url = "https://www.target.com/c/chairs-living-room-furniture/-/N-5xtlz?limit=5&Nao=0"
 
-for link in links:
-    prod_page = browser.get(link)
-    for i in range(1, 1):
-        browser.execute_script("window.scrollBy(0, 500);")
-        time.sleep(1)
-    browser.find_element_by_xpath('//*[@id="tabContent-tab-details"]/div/div[3]/button').click()
-    product_infos.append(get_product_info(prod_page))
+next_page = True
+while next_page :
+    product_infos.append(scrape_PLP(base_url))
+    print(product_infos)
 
-print(product_infos)
+    # Try to find the next PLP page and go to it
+    try:
+        browser.get(base_url)
+        base_url = browser.find_element_by_xpath('//a[@data-test="next"]').get_attribute("href")
+        print(base_url)
+    except:
+        print("no next page")
+        next_page = False
 
-# Try to find the next PLP page and go to it
-try:
-    next_url = browser.find_element_by_xpath('//a[@data-test="next"]').get_attribute("href")
-    print(next_url)
-    browser.get(next_url)
-except:
-    print("no next page")
-
+# Write to a file
+# file_path = '~/Desktop/workfile.txt'
+# with open('workfile', 'w') as f:
+#     json.dump(product_infos, f)
+# f.closed
 
 browser.quit()
